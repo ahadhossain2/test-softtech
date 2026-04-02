7f54770664157cc20033539e6bfa90f20d66120c
Md Ahad Hossain <hossanahad9@gmail.com>
2026-04-03 03:54:42 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Header/Navbar";
+import Footer from "./Components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer mb-16">
+      <p>© {new Date().getFullYear()} Sofftech. All rights reserved.</p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus ut
+        quae minima deserunt aperiam tempora omnis, rem sed debitis. Voluptatum
+        dolore explicabo harum ut ipsam ab repellat amet dolores at!
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
